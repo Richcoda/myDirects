619d45a2d37242c7757cd0376d3976e97766a118
“richmond” <richmond@osspesecuritycenter.com>
2025-01-20 23:27:35 -0800

    updated code

--- api/redirector.js
+++ api/redirector.js
@@ -11,6 +11,13 @@ module.exports = (req, res) => {
             // Decode the URL (the encrypted URL)
             const decodedUrl = decodeURIComponent(encryptedUrl);
 
+            // Validate the decoded URL (optional step for extra security)
+            try {
+                new URL(decodedUrl); // This will throw an error if the URL is invalid
+            } catch (e) {
+                return res.status(400).send('Bad Request: Invalid URL format.');
+            }
+
             // Send the redirect response with status 301 (permanent redirect)
             res.writeHead(301, { 'Location': decodedUrl });
             res.end();
